866b444aa5e0d2a6bcbfbc427ab6a8cb481b3a80
Shevela_Denis <shevela83@mail.ru>
2023-03-07 09:34:13 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,60 @@
+﻿// **Задача 48:** Задайте 
+// двумерный массив размера m на n, каждый элемент
+//  в массиве находится по формуле: A[i][j] = i + j. 
+//Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+using System;
+using static System.Console;
+Console.Clear();
+Write("Ввудите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Ввудите количество столбцов массива: ");
+int column = int.Parse(ReadLine());
+int [,] muss = GetArray(rows, column, 3, 4);
+FealArray(muss);
+PrintArray(muss);
+void FealArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            inArray[i,j] = i+j;
+        }
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+ 
+
+
+
